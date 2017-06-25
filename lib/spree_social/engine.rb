4d9c34c68e2b062953fcf5d86ab58d7393490c00
Gundsambuu <ssxenon01@gmail.com>
2017-06-25 19:38:48 +0900

    Update engine.rb

--- lib/spree_social/engine.rb
+++ lib/spree_social/engine.rb
@@ -40,7 +40,7 @@ module SpreeSocial
 
   def self.setup_key_for(provider, key, secret)
     Devise.setup do |config|
-      config.omniauth provider, key, secret, setup: true, scope: 'email', info_fields: 'email, name'
+      config.omniauth provider, key, secret, setup: true, scope: 'email, public_profile', info_fields: 'email, name, gender, first_name, last_name, age_range'
     end
   end
 end
